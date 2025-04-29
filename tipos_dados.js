f41b6d1304156268a5878dda4444c89c9c1bd27f
CAIQUE BATISTA LINO <caiquelino6183@digitalcollege.internal>
2025-04-29 20:08:44 -0300

    comitando aula em JavaScript

---
+++ tipos_dados.js
@@ -0,0 +1,28 @@
+//var, const e let
+
+//1-const (constante)
+const pi = 3.14;
+//imprimindo a variavel no console
+console.log(pi);
+
+//2 - ket (o valor da variável pode ser alterado)
+let nome_atualizado = 'Caique'
+//console.log(nome)
+
+nome = 'Caique Lindão!';
+console.log(nome);
+
+let desconto = 20.5;
+//multiplicando a variavel, sem alterar o valor original
+console.log(2*desconto)
+
+//alterar valor da variável
+desconto = desconto*2
+console.log('Desconto atualizado: ' + desconto)
+
+//Array (lista)
+let tamanhos = ['PP, P, M, G, XG'];
+
+let temPet = true;
+
+const idade = 28;
